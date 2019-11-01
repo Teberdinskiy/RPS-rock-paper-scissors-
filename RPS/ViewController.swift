fc4d3369ad40de6f0fb7878b14c20738ee32f5f9
Alikhan Batchaev <loko-han@mail.ru>
2019-11-01 18:49:44 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Алихан Батчаев on 01.11.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
